4aabba3d19988d16f4b3b60723fc8f8aa569b44a
sopharsogood <justanothersophie@gmail.com>
2021-07-20 18:50:09 -0400

    commit tree e3eb3af6de6e88ec7bcd3cd656d013f0fdc43cee

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    final_entry = array.pop
+    array.join(", ") + ", and " + final_entry
+  elsif array.size == 1
+    array[0]
+  elsif array.size == 2
+    array[0] + " and " + array[1]
+  end
 end
